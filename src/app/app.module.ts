fb4bfc5ffb51ce0ff29f8d4d7ed18fe87ceef11e
olena.mordas <olena.mordas@synpulse.com>
2021-06-09 09:16:16 +0200

    optimized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ToolbarComponent } from './component/toolbar/toolbar.component';
-import { CardComponent } from './component/card/card.component';
-import { AccordionComponent } from './component/accordion/accordion.component';
-import { ProgressBarComponent } from './component/progress-bar/progress-bar.component';
-import { StarRatingComponent } from './component/star-rating/star-rating.component';
-import { PageTopComponent } from './component/page-top/page-top.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { APP_ROUTES } from 'src/app.routes';
+import { AppComponent } from './app.component';
+import { AccordionComponent } from './component/accordion/accordion.component';
+import { CardComponent } from './component/card/card.component';
 import { ComponentDocsComponent } from './component/component-docs/component-docs.component';
-import { PipeDocsComponent } from './pipes/pipe-docs/pipe-docs.component';
-import { ServiceDocsComponent } from './services/service-docs/service-docs.component';
-import { DirectiveDocsComponent } from './directives/directive-docs/directive-docs.component';
+import { PageTopComponent } from './component/page-top/page-top.component';
+import { ProgressBarComponent } from './component/progress-bar/progress-bar.component';
+import { StarRatingComponent } from './component/star-rating/star-rating.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+
 
 @NgModule({
   declarations: [
