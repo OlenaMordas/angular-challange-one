86815e39c0f8d4233a222e236fcb978c4bb017c3
olena.mordas <olena.mordas@synpulse.com>
2021-06-18 11:06:50 +0200

    .

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { User } from './user.model';
 import {map} from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class UserService implements OnInit {
   ngOnInit(): void {
   }
 
-  getUserById(id:number){
+  getUserById(id:number): Observable<User>{
    return this.http.get(`${this.baseRoute}/${id}`).pipe(
      map((value)=>new User(value))
    );    
