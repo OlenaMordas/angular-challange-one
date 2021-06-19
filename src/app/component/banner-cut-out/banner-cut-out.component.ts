fbff3d1374d5ae411c04a3abd2b4a0021ca03889
olena.mordas <olena.mordas@synpulse.com>
2021-06-19 11:34:42 +0200

    banner cut out

---
+++ src/app/component/banner-cut-out/banner-cut-out.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'banner-cut-out',
+  templateUrl: './banner-cut-out.component.html',
+  styleUrls: ['./banner-cut-out.component.scss']
+})
+export class BannerCutOutComponent {
+
+  @Input()
+  public imgUrl = 'https://images.unsplash.com/photo-1509478074354-0de2c9bb7c8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2173&q=80';
+
+  @Input()
+  public title = '100 Angular challenges';
+}
